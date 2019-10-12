32df79e214e0bd35f5f31573e17368c2f277552d
Pritish Puhan <54423565+pritishpuhan@users.noreply.github.com>
2019-10-12 14:16:18 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,3 +17,10 @@ pch =2,
 col = "red",
 lwd = 3))
 dev.off() ## Close the graphical device
+
+          
+## Answer
+Total.Emissions.2008 <- Total.Emissions.yearwise[Total.Emissions.yearwise$year == 2008, 2]
+Total.Emissions.1999 <- Total.Emissions.yearwise[Total.Emissions.yearwise$year == 1999, 2]
+
+delta.Total.Emissions <- Total.Emissions.2008 - Total.Emissions.1999
