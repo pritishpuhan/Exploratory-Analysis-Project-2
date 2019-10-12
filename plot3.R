1f4a0d47bc95dfe55b3859306a1e9d416318130e
Pritish Puhan <54423565+pritishpuhan@users.noreply.github.com>
2019-10-12 14:13:19 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,19 @@
+## Read the file
+SCC <- readRDS("C:/Users/Pritish Puhan/Downloads/exdata_data_NEI_data/summarySCC_PM25.rds")
+tot.emissions.type <- SCC %>% 
+    subset(fips == "24510") %>%
+    group_by(year, type) %>%
+    summarize(Total.Emissions.Type = sum(Emissions, na.rm = TRUE))
+
+emissions.type <- ggplot(data = tot.emissions.type, aes(year, Total.Emissions.Type))
+
+emissions.type <- emissions.type + 
+    geom_point(color = "red", 
+               size = 4, 
+               alpha = 1/3) + 
+    facet_grid(. ~ type) +
+    xlab("Year") +
+    ylab("Total Emissions [Tons]") +
+    ggtitle("Total Annual Emissions in Baltimore by Year")
+
+emissions.type
