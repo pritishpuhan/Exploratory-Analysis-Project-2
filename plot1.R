3fcd8b3c9c7cc9c6fad4348f13de875d5ea5d7b1
Pritish Puhan <54423565+pritishpuhan@users.noreply.github.com>
2019-10-12 10:51:04 +0530

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,19 @@
+## Read the file
+SCC <- readRDS("C:/Users/Pritish Puhan/Downloads/exdata_data_NEI_data/summarySCC_PM25.rds")
+## Summarize Total Emissions year wise
+Total.Emissions.yearwise <- SCC %>%
+group_by(year) %>%
+summarize(Total.Emissions = sum(Emissions, na.rm =T)
+png(filename = "plot1.png") ## Prepare the png file
+# Plot the relationship
+with(Total.Emissions.yearwise,
+plot(x = year,
+y = Total.Emissions,
+ylab = "Total Annual emissions"
+xlab = "year"
+main = "Total Emissions in US year wise"
+cex = 2,
+pch =2,
+col = "red",
+lwd = 3))
+dev.off() ## Close the graphical device
